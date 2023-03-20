c40a1acd5c900be38a8699adde44842533337bc6
BogdanSssS <bogdansavin315@gmail.com>
2023-03-20 20:17:50 +0200

    Added icons to NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import style from "./NavBar.module.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { faMusic } from '@fortawesome/free-solid-svg-icons'
+import { faHandshake } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const NavBar = () => {
    return (
       <div className={`${style.nav}`}>
          <div className={`${style.nav__container}`}>
             <ul className={`${style.nav__menuList}`}>
-               <li className={`${style.nav__menuLink}`}><a href="#">Home</a></li>
-               <li className={`${style.nav__menuLink}`}><a href="#">Messeges</a></li>
-               <li className={`${style.nav__menuLink}`}><a href="#">Music</a></li>
-               <li className={`${style.nav__menuLink}`}><a href="#">Friend`s</a></li>
+               <li className={`${style.nav__menuLink}`}>
+                  <a href="#"> <FontAwesomeIcon className={`${style.nav__icon}`} icon={faHouse} /> Home</a>
+               </li>
+               <li className={`${style.nav__menuLink}`}>
+                  <a href="#"> <FontAwesomeIcon className={`${style.nav__icon}`} icon={faEnvelope} /> Messeges</a></li>
+               <li className={`${style.nav__menuLink}`}>
+                  <a href="#"> <FontAwesomeIcon className={`${style.nav__icon}`} icon={faMusic} /> Music </a>
+               </li>
+               <li className={`${style.nav__menuLink}`}>
+                  <a href="#"> <FontAwesomeIcon className={`${style.nav__icon}`} icon={faHandshake} /> Friend`s </a>
+               </li>
             </ul>
          </div>
       </div>
