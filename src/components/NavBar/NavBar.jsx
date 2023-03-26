b8d6a265821cf65b45a790fda85b344a9a29c32c
BogdanSssS <bogdansavin315@gmail.com>
2023-03-26 16:53:12 +0300

    Added structure to News page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import style from "./NavBar.module.scss";
+
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -2,11 +2,17 @@ import style from './News.module.scss';
 
 const News = (props) => {
    return(
-      <section className={`${style.page}`}>
+      <div className={`${style.page}`}>
          <div className={`${style.page__container}`}>
-            THIS IS <span>NEWS</span> PAGE
+            <div className={`${style.page__news1}`}>news 1</div>
+            <div className={`${style.page__news2}`}>news 2</div>
+            <div className={`${style.page__content}`}>content</div>
+            <div className={`${style.page__calendar}`}>calendar</div>
+            <div className={`${style.page__weather}`}>weather</div>
+            <div className={`${style.page__news3}`}>news 3</div>
+            <div className={`${style.page__friendsNews}`}> friends news</div>
          </div>
-      </section>
+      </div>
    )
 }
 
